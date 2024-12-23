cac83a76d090d5d8ed0322bbd91d5aa70e3d96cc
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-12-23 22:23:16 +0200

    new better UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Lexend } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lexend = Lexend({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Customize weights as needed
 });
 
 export const metadata: Metadata = {
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" className={lexend.className}>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,227 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Flex vertical align="center">
+    <div className="main-container">
+      <Flex justify="center">
+        <Flex
+          className="wrapper-nav"
+          style={{
+            width: "75%",
+            // backgroundColor: "blue",
+            padding: "15px 0",
+            // borderBottom: "1px solid white",
+          }}
+          align="center"
+          justify="space-between"
+        >
+          <Flex gap={10} align="center">
+            <Flex
+              style={{
+                padding: "6px 11px",
+                fontSize: 25,
+                fontWeight: 700,
+                backgroundColor: "#48f7a8",
+                borderRadius: "50%",
+                color: "#007afa",
+              }}
+            >
+              A
+            </Flex>{" "}
+            <span style={{ fontSize: 20, fontWeight: 600, color: "white" }}>
+              ASAC
+            </span>
+          </Flex>
+          {/* <Flex style={{ color: "white" }} gap={30}>
+            <span>Home</span> <span>About us</span> <span>Our Team</span>{" "}
+            <span>Pricing</span> <span>Blog</span> <span>Contact</span>
+          </Flex> */}
+          <Flex gap={20}>
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                fontWeight: 600,
+                color: "white",
+              }}
+            >
+              Login
+            </button>
+            <button
+              style={{
+                border: "none",
+                borderRadius: "20px",
+                backgroundColor: "#48f7a8",
+                padding: "15px 20px",
+                fontWeight: 600,
+              }}
+            >
+              Sign Up free
+            </button>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex style={{ padding: "30px 0" }} gap={30} vertical>
+        <Flex vertical align="center">
+          <Flex
+            vertical
+            align="center"
+            gap={20}
+            style={{ width: "60%", color: "white" }}
+          >
+            <Flex
+              justify="center"
+              style={{ fontSize: 55, textAlign: "center" }}
+            >
+              Done right, every time.
+            </Flex>
+            <Flex style={{ textAlign: "center", lineHeight: 1.6 }}>
+              Our domestic services platform instantly connects you with skilled
+              professionals — no guesswork, clear prices, and transparent
+              progress — ensuring stress-free services at your fingertips.
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex justify="center">
+          <Flex
+            style={{ backgroundColor: "#f7f7f7", width: "60%", height: 400 }}
+            justify="center"
+            align="center"
+          >
+            Demo
+          </Flex>
+        </Flex>
+        <Flex align="center" style={{ position: "relative" }} vertical gap={20}>
+          <Image
+            style={{ position: "absolute", top: -20, left: "39%" }}
+            src={"/image.png"}
+            width={30}
+            height={65}
+            alt=""
+          />
+          <span style={{ fontSize: 23, color: "white", fontWeight: 600 }}>
+            Coming soon ...
+          </span>
+          <button
+            style={{
+              border: "none",
+              borderRadius: "20px",
+              backgroundColor: "#48f7a8",
+              padding: "15px 20px",
+              fontWeight: 600,
+              width: 250,
+            }}
+          >
+            Register here
+          </button>
+        </Flex>
+        {/* <Flex>
+          <Flex gap={30} align="center" style={{ width: "25%" }} vertical>
+            <Flex>
+              <Image
+                style={{ borderRadius: 20 }}
+                src={"/poza1.png"}
+                height={280}
+                width={280}
+                alt=""
+              />
+            </Flex>
+            <Flex justify="center" style={{ color: "white" }}>
+              <Flex gap={7} style={{ padding: 10 }} vertical>
+                <span style={{ fontSize: 30 }}>15+</span>
+                <span style={{ fontSize: 12 }}>Years of experience</span>
+              </Flex>
+              <Flex gap={7} style={{ padding: 10 }} vertical>
+                <span style={{ fontSize: 30 }}>84k</span>
+                <span style={{ fontSize: 12 }}>Customers worldwide</span>
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <Flex gap={20} align="center" style={{ width: "50%" }} vertical>
+            <Flex align="center" gap={50}>
+              <button
+                style={{
+                  border: "none",
+                  borderRadius: "20px",
+                  backgroundColor: "#48f7a8",
+                  padding: "15px 20px",
+                  fontWeight: 600,
+                }}
+              >
+                Get Trial Project
+              </button>
+              <span style={{ color: "white" }}>Watch Demo</span>
+            </Flex>
+            <Flex>
+              {" "}
+              <Image
+                style={{ borderRadius: 20 }}
+                src={"/poza2.png"}
+                height={370}
+                width={600}
+                alt=""
+              />
+            </Flex>
+          </Flex>
+
+          <Flex gap={30} align="center" style={{ width: "25%" }} vertical>
+            <Flex>
+              <Image
+                style={{ borderRadius: 20 }}
+                src={"/poza3.png"}
+                height={280}
+                width={280}
+                alt=""
+              />
+            </Flex>
+            <Flex justify="center" style={{ color: "white" }}>
+              <Flex style={{ padding: 10, position: "relative" }}>
+                <Image
+                  style={{
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    marginRight: -15, // Overlaps the next image
+                    zIndex: 3, // Ensures proper layering
+                  }}
+                  src={"/om1.png"}
+                  height={50}
+                  width={50}
+                  alt=""
+                />
+                <Image
+                  style={{
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    marginRight: -15, // Overlaps the next image
+                    zIndex: 2, // Slightly lower in the stack
+                  }}
+                  src={"/om2.png"}
+                  height={50}
+                  width={50}
+                  alt=""
+                />
+                <Image
+                  style={{
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    zIndex: 1, // Lowest in the stack
+                  }}
+                  src={"/om3.jpg"}
+                  height={50}
+                  width={50}
+                  alt=""
+                />
+              </Flex>
+
+              <Flex gap={7} style={{ padding: 10 }} vertical>
+                <span style={{ fontSize: 30 }}>140k</span>
+                <span style={{ fontSize: 12 }}>Happy Customers</span>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex> */}
+      </Flex>
+      {/* <Flex vertical align="center">
         <Flex
           align="center"
           style={
@@ -190,7 +409,7 @@ export default function Home() {
             </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      </Flex> */}
     </div>
   );
 }
